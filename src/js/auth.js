f87468a7741b0e419ba36998fdf80ec6ee3e05ad
sainamercy <sainamercy@gmail.com>
2022-12-15 12:56:50 +0300

    authentication-functionality

---
+++ src/js/auth.js
@@ -0,0 +1,56 @@
+function authentication(){
+    // getting elements from the DOM
+const authSection = document.querySelector("#authentication")
+const signUpForm = document.querySelector("#signUp");
+const loginForm = document.querySelector("#login");
+const signUpEmail = document.querySelector("#signupEmail");
+const signUpUserName = document.querySelector("#signupUserName");
+const signUpPassword = document.querySelector("#signUpPass");
+const loginEmail = document.querySelector("#loginEmail");
+const loginPassword = document.querySelector("#loginPass");
+const mainPage = document.querySelector("#main");
+// hide login form and main section
+loginForm.style.display = "none";
+mainPage.style.display = "none";
+
+// initializing signup details to be derived from user input
+let signDetails;
+
+// signup function
+function signup() {
+  signDetails = {
+    email: signUpEmail.value,
+    userName: signUpUserName.value,
+    password: signUpPassword.value,
+  };
+  signUpForm.style.display = "none";
+  loginForm.style.display = "flex";
+  console.log(signDetails);
+  return signDetails;
+}
+// executing signup on submit of the form
+signUpForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  signup();
+});
+
+// login function 
+function login(e) {
+  e.preventDefault();
+
+  console.log(signDetails);
+  const email = loginEmail.value;
+  const password = loginPassword.value;
+
+//   using information from signup to login user
+  if (email === signDetails.email && password === signDetails.password) {
+    authSection.style.display = "none";
+    mainPage.style.display = "flex";
+  }
+}
+
+// executing login function on submit of the form
+loginForm.addEventListener("submit", login);
+}
+authentication()
---
+++ src/js/index.js
@@ -0,0 +1,3 @@
+// import authentication from "./auth";
+
+// authentication()
