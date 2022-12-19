91f84b6c1e8bd5ac0862552894ff293dff720fad
sainamercy <sainamercy@gmail.com>
2022-12-19 11:47:49 +0300

    added-spinner

--- src/js/renderNews.js
+++ src/js/renderNews.js
@@ -5,6 +5,14 @@ export async function renderNews() {
   const newsListContainer = document.querySelector("#newsList");
   const categoryListContainer = document.querySelector("#categoryList");
 
+  // loading spinner
+  function renderSpinner(parentElement){
+    parentElement.innerHTML =`<i class="fa-solid fa-spinner fa-spin"></i>`
+  }
+  // clear parent elements before appending children
+  function clear(parentElement){
+    parentElement.innerHTML= ""
+  }
   // display category list
   let newsCategory = "all";
   const allResponse = await fetch(FETCH_URL + newsCategory);
@@ -18,9 +26,11 @@ export async function renderNews() {
       // getting news category, then fetch data according to category
       const list = categoryListContainer.querySelector("li");
       list.addEventListener("click", async (e) => {
+        renderSpinner(newsListContainer)
         newsCategory = e.target.textContent;
         const response = await fetch(FETCH_URL + newsCategory);
         const data = await response.json();
+        clear(newsListContainer)
         getNewsTitles(data, newsCategory);
       });
     });
@@ -32,7 +42,7 @@ export async function renderNews() {
     const briefsSpan = document.querySelector("#briefsSpan");
     data.data.map((news) => {
       const markUp = `<li><img src="${news.imageUrl}" alt="news-poster"><h3>${news.title}</h3><p><i>by: ${news.author}</i></p><p>${news.date}</p></li>`;
-      briefsSpan.textContent = `${category}:`;
+      briefsSpan.textContent = `${category.toUpperCase()}:`;
       newsListContainer.insertAdjacentHTML("afterbegin", markUp);
     });
   }
